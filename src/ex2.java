6d1aa61c07a3861a54eebe50c92ca5103626936e
jillxx <jiaoxu84@gmail.com>
2018-12-11 00:18:09 -0500

    first commit

---
+++ src/ex2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ex2 {
+
+	public static void main(String[] args) {
+		System.out.println("ENter a number:  <<");
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt() + 1;
+		System.out.println(num);
+	}
+
+}
---
+++ src/helloworldEX1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class helloworldEX1 {
+
+	public static void main(String[] args) {
+		System.out.print("Enter some text: ");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		System.out.println(input);
+
+		
+	}
+
+}
